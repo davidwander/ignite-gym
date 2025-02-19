5f845b505d06b29d2e956d71b6fa8805a05d930d
David <dwander616@gmail.com>
2025-02-19 16:47:00 -0300

    adicionando gluestack config

--- App.tsx
+++ App.tsx
@@ -5,11 +5,13 @@ import {
   Roboto_400Regular,
 } from '@expo-google-fonts/roboto';
 import { GluestackUIProvider, Text } from '@gluestack-ui/themed';
+
+import { config } from '@gluestack-ui/config'
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <GluestackUIProvider>
+    <GluestackUIProvider config={config}>
       <View 
       style={{ 
         flex: 1, 
