e4194362c5766379575715fa5a26d8551295d22c
David <dwander616@gmail.com>
2025-02-22 19:11:35 -0300

    alterando icones da barra bottom de navegação

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,11 +2,16 @@ import {
   createBottomTabNavigator, 
   BottomTabNavigationProp 
 } from "@react-navigation/bottom-tabs";
+import { gluestackUIConfig } from "../../config/gluestack-ui.config";
 
 import { Home } from "@screens/Home";
 import { Profile } from "@screens/Profile";
 import { History } from "@screens/History";
 import { Exercise } from "@screens/Exercise";
+
+import HomeSvg from "@assets/home.svg";
+import HistorySvg from "@assets/history.svg";
+import ProfileSvg from "@assets/profile.svg";
 
 type AppRoutes = {
   home: undefined,
@@ -20,11 +25,38 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
+  const { tokens } = gluestackUIConfig
+  const iconSize = tokens.space["8"]
+
   return (
-    <Navigator>
-      <Screen name="home" component={Home} />
-      <Screen name="history" component={History} />
-      <Screen name="profile" component={Profile} />
+    <Navigator screenOptions={{
+      headerShown: false,
+      tabBarShowLabel: false,
+    }}>
+      <Screen 
+        name="home" 
+        component={Home} 
+        options={{ tabBarIcon: ({ color }) => (
+          <HomeSvg fill={color} width={iconSize} height={iconSize} />
+        ),
+      }}
+      />
+      <Screen 
+        name="history" 
+        component={History} 
+        options={{ tabBarIcon: ({ color }) => (
+          <HistorySvg fill={color} width={iconSize} height={iconSize} />
+        ),
+      }}
+      />
+      <Screen 
+        name="profile" 
+        component={Profile}
+        options={{ tabBarIcon: ({ color }) => (
+          <ProfileSvg fill={color} width={iconSize} height={iconSize} />
+        ),
+      }}
+      />
       <Screen name="exercise" component={Exercise} />
     </Navigator>
   )
