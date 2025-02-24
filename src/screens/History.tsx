64b57fd3d1d4dccd4bdc0b2fc294de9675b90732
David <dwander616@gmail.com>
2025-02-24 10:56:41 -0300

    componente HistoryCard

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,11 +1,14 @@
 import { VStack } from "@gluestack-ui/themed";
 
 import { ScreenHeader } from "@components/ScreenHeader";
+import { HistoryCard } from "@components/HistoryCard";
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico" />
+
+      <HistoryCard />
     </VStack>
   )
 }
