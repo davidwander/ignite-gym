acf6979f77439dd1ec2bfc07eccb8df887ce85b5
David <dwander616@gmail.com>
2025-02-24 10:15:26 -0300

    Listando exercicios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,6 +7,12 @@ import { Group } from "@components/Group";
 import { ExerciseCard } from "@components/ExerciseCard"
 
 export function Home() {
+  const [exercises, setExercises] = useState([
+    "Puxada frontal", 
+    "Remada curvada", 
+    "Remada unilateral", 
+    "Levantamento terra",
+  ]);
   const [groups, setGroups] = useState(["Costas", "Biceps", "Triceps", "Ombro"]);
   const [groupSelected, setGroupSelected] = useState("Costas");
 
@@ -20,7 +26,7 @@ export function Home() {
         renderItem={({ item }) => (
           <Group 
             name={item}
-            isActive={groupSelected === item}
+            isActive={groupSelected.toLowerCase() === item.toLowerCase()}
             onPress={() => setGroupSelected(item)} 
           />
         )}
@@ -30,18 +36,24 @@ export function Home() {
         style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
       />
 
-      <VStack px="$8">
+      <VStack px="$8" flex={1}>
         <HStack justifyContent="space-between" mb="$5" alignItems="center">
           <Heading color="$gray200" fontSize="$md" fontFamily="$heading">
             Exercícios
           </Heading>  
           
           <Text color="$gray200" fontSize="$sm" fontFamily="$body">
-            4
+            {exercises.length}
           </Text>  
         </HStack>
 
-        <ExerciseCard />
+        <FlatList 
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={(() => <ExerciseCard />)}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 20 }}
+        />
       </VStack>
     </VStack>
   )
