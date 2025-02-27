8fb17cc945128348298c695427f345363f5b477b
David <dwander616@gmail.com>
2025-02-27 18:06:45 -0300

    utilizando interceptors, criando AppError

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,15 @@
 import axios from "axios";
 
- export const api = axios.create({
+  const api = axios.create({
   baseURL: "http://192.168.3.7:3333"
+});
+
+api.interceptors.request.use((config) => {
+  console.log("INTERCEPTOR => ", config);
+
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 })
+
+export { api };
