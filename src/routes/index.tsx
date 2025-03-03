5cd8e7e1cae75087b5921c53554b0d0b2e3fadac
David <dwander616@gmail.com>
2025-03-03 20:53:53 -0300

    criando hook

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,7 @@
-import { useContext } from "react";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { Box } from "@gluestack-ui/themed";
 
-import { AuthContext } from '@contexts/AuthContext';
+import { useAuth } from "@hooks/useAuth";
 
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
@@ -10,8 +9,9 @@ import { AuthRoutes } from "./auth.routes";
 import { gluestackUIConfig } from "../../config/gluestack-ui.config";
 
 export function Routes() {
-  const contextData = useContext(AuthContext);
-  console.log("Usuário logado => ", contextData)
+  const { user } = useAuth();
+  
+  console.log("Usuário logado => ", user)
 
   const theme = DefaultTheme
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700;
