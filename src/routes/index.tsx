6786e1818bd24ae0d606ce571fc9e85d7e9a6f7a
David <dwander616@gmail.com>
2025-03-05 12:15:25 -0300

    Redirecionando o usuário logado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -20,7 +20,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   )
