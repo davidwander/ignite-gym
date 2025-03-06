512477de00561fe9450ccb5c264bf47df30e6f49
David <dwander616@gmail.com>
2025-03-06 17:49:43 -0300

    Criando conta e logando usuário

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { 
   VStack, 
   Image, 
@@ -23,6 +24,7 @@ import { AppError } from "@utils/AppError";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { useAuth } from "@hooks/useAuth";
 
 type FormDataProps = {
   name: string;
@@ -45,7 +47,10 @@ const signUpSchema = yup.object({
 })
 
 export function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
+
   const toast = useToast();
+  const { signIn } = useAuth();
 
   const { 
     control, 
@@ -63,9 +68,14 @@ export function SignUp() {
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
     try {
-      const response = await api.post('/users', { name, email, password });
-      console.log(response.data); 
+      setIsLoading(true);
+
+      await api.post('/users', { name, email, password });
+      await signIn(email, password);
+      
     } catch (error) {
+      setIsLoading(false);
+
       const isAppError = error instanceof AppError;
       const title = isAppError ? error.message : "Ocorreu um erro inesperado";
 
@@ -167,6 +177,7 @@ export function SignUp() {
             <Button 
               title="Criar e acessar"
               onPress={handleSubmit(handleSignUp)} 
+              isLoading={isLoading}
             />
           </Center>
 
