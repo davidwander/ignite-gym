f132ba9e9ff250346088d658beb8911e04705d78
David <dwander616@gmail.com>
2025-03-06 18:42:34 -0300

    Anexando token nas requisições

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import  { createContext, ReactNode, useEffect, useState } from "react";
 
+import { storageAuthTokenSave } from "@storage/storageAuthToken"
 import { storageUserSave, storageUserGet, storageUserRemove } from "@storage/storageUser";
 
 import { api } from "@services/api";
@@ -23,6 +24,23 @@ export const AuthContext = createContext<AuthContextDataProps>({} as AuthContext
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
+
+  async function storageUserAndToken(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true);
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+   
+      await storageUserSave(userData);
+      await storageAuthTokenSave(token);
+      setUser(userData);
+
+      } catch (error) {
+        throw error;
+      } finally {
+        setIsLoadingUserStorageData(false);
+      }
+  }
   
 
   async function signIn(email: string, password: string){
@@ -30,8 +48,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       const { data } = await api.post('/sessions', { email, password });
       
       if (data.user && data.token) {
-        setUser(data.user);
-        storageUserSave(data.user);
+        storageUserAndToken(data.user, data.token);
       }
     } catch (error) {
       throw error;
