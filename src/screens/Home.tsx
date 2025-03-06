209b1dc4031e4f26ae1842da5155026079c54940
David <dwander616@gmail.com>
2025-03-06 19:43:30 -0300

    buscando e listando por grupo

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FlatList } from "react-native"
-import { Heading, HStack, VStack, Text } from "@gluestack-ui/themed";
+import { Heading, HStack, VStack, Text, useToast, Box } from "@gluestack-ui/themed";
+
+import { api } from "@services/api";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
 import { ExerciseCard } from "@components/ExerciseCard"
 import { useNavigation } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
+import { AppError } from "@utils/AppError";
 
 
 export function Home() {
@@ -16,14 +19,40 @@ export function Home() {
     "Remada unilateral", 
     "Levantamento terra",
   ]);
-  const [groups, setGroups] = useState(["Costas", "Biceps", "Triceps", "Ombro"]);
+  const [groups, setGroups] = useState<string[]>([]);
   const [groupSelected, setGroupSelected] = useState("Costas");
 
+  const toast = useToast()
   const navigation = useNavigation<AppNavigatorRoutesProps>()
 
   function handleOpenExerciseDetails() {
     navigation.navigate("Exercise")
   }
+
+  async function fetchGroups() {
+    try {
+      const response = await api.get("/groups")
+      setGroups(response.data)
+
+      
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError ? error.message : "Erro ao buscar grupos";
+      
+      toast.show({
+        render: () => (
+          <Box bg="$red500" mt="$16" px="$4" py="$4" rounded="$sm">
+            <Text color="$white">{title}</Text>
+          </Box>
+        ),
+        placement: "top"
+      })
+    }
+  }
+
+  useEffect(() => {
+    fetchGroups()
+  }, [])
 
   return (
     <VStack flex={1}>
